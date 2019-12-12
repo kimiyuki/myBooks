ba9c7076246216adc709aeaf5db14aefde52a6db
kimiyuki shirai <kimiyuki@gmail.com>
2019-12-12 20:33:36 +0900

    feature: take pic and save it in Gyazo

--- src/front.ts
+++ src/front.ts
@@ -30,23 +30,28 @@ function doGet(e: any): GoogleAppsScript.HTML.HtmlOutput {
 //   "get_image_url" : "https://gyazo.com/api/upload/8980c52421e452ac3355ca3e5cfe7a0c",
 //   "expires_at" : 1401178164
 // }
-const getGyazoImageUrl = (dataUrl: string, refererUrl: string): string => {
+function getGyazoImageUrl(dataUrl: string, refererUrl: string): string {
   // https://gyazo.com/api/docs/image
   const gyazoUrl = "https://upload.gyazo.com/api/upload/easy_auth";
-  console.log(dataUrl);
-  const body = JSON.stringify({
+  const body = {
     client_id: ScriptProperties.getProperty("GYAZO_CLIENT_ID"),
     image_url: dataUrl,
     referer_url: encodeURIComponent(refererUrl),
     title: "foo book title"
-  });
-  const res = UrlFetchApp.fetch(gyazoUrl, {
-    contentType: "application/json; charset=utf-8",
-    method: "post"
-  });
+  };
+  const params: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+    //shouled be default, contentType: "application/x-www-form-urlencoded' 
+    method: "post",
+    payload: body
+    // tslint:disable-next-line: object-literal-sort-keys
+    //muteHttpExceptions: true
+  };
+  Logger.log(UrlFetchApp.getRequest(gyazoUrl, params));
+  const res = UrlFetchApp.fetch(gyazoUrl, params)
+  Logger.log(res.getResponseCode);
   const jsn = JSON.parse(res.getContentText());
   return jsn.get_image_url;
-};
+}
 
 function runBackEnd() {
   return "hello from backend";
