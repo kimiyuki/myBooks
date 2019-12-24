df1c359557fdc1381a2fdf41810e269d5aef25fe
kimiyuki shirai <kimiyuki@gmail.com>
2019-12-24 17:02:41 +0900

    feature: add tiimeStamp

---
+++ src/onEdit.ts
@@ -0,0 +1,16 @@
+// https://developers.google.com/apps-script/guides/triggers/events
+// https://community.glideapps.com/t/time-stamp-update/1382/11
+// https://webapps.stackexchange.com/questions/119684/diference-between-onedit-or-onchange-trigger
+
+function onEdit(e: GoogleAppsScript.Events.SheetsOnEdit) {
+  const rg = e.range;
+  if (rg.getSheet().getName() === "books") {
+    const row = rg.getRow();
+    const col = rg.getColumn();
+    if (col !== 11) {
+      rg.getSheet()
+        .getRange(row, 11)
+        .setValue(new Date());
+    }
+  }
+}
