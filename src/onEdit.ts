6faddaeb661cc8f807fab8b56f5324e4a6f7805b
kimiyuki shirai <kimiyuki@gmail.com>
2019-12-30 14:56:10 +0900

    add deleteRow to be triggered periodically Because GlideApps only clear data instead to delete. https://docs.glideapps.com/all/guides/quick-starts/intermediate-techniques/adding-editing-and-deleting#deleting

--- src/onEdit.ts
+++ src/onEdit.ts
@@ -7,24 +7,40 @@ function onEdit(e: GoogleAppsScript.Events.SheetsOnEdit) {
   if (rg.getSheet().getName() === "books") {
     const row = rg.getRow();
     const col = rg.getColumn();
-    if (col !== 11) {
+    if (col < 11) {
       rg.getSheet()
         .getRange(row, 11)
         .setValue(new Date());
     }
   }
 }
-
-function onChange(e) {
-  Logger.log(e.changeType);
-  Logger.log(e);
+function deleteEmptyRows() {
+  const sh = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("books");
+  const n = sh.getLastRow();
+  // as deleting row get renumbered so from bottom to top is a better strategy..
+  for (let i = n; i > 1; i--) {
+    if (sh.getRange(i, 1).getValue() === "") {
+      sh.deleteRow(i);
+    }
+  }
 }
 
+function onChange(e) {
+  console.info(e.changeType);
+  console.info(e.toString());
+  console.info(SpreadsheetApp.getActiveRange().getA1Notation());
+  const sh = SpreadsheetApp.getActiveSheet();
+  const rowNum = SpreadsheetApp.getActiveRange().getRow();
+  if (sh.getName() === "books" && sh.getRange(rowNum, 1).getValue() === "") {
+    // pending
+    // sh.deleteRow(rowNum);
+    // console.info(`delete row ${rowNum} in 'books'`);
+  }
+}
 
 function createSpreadsheetChangeTrigger() {
-  const ss = SpreadsheetApp.getActive();
-  ScriptApp.newTrigger('onChange')
-    .forSpreadsheet(ss)
+  ScriptApp.newTrigger("onChange")
+    .forSpreadsheet(SpreadsheetApp.getActive())
     .onChange()
     .create();
 }
